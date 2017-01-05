7934e6c82fa0d65c36c88a55b99086daad96acc5
sivaprasad vindula <sivaprasad.vindula@gmail.com>
2017-01-05 14:45:11 +0530

    pages changes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,24 +5,28 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     #get home_url
     get home_path
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     #get help_url
     get help_path
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     #get about_url
     get about_path
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact" do
     #get contact_url
     get contact_path
     assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
